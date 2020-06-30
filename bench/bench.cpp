54885d862d66395a7c066eb7f3c397975874f832
Matthias Seiffert <matthias-seiffert@hotmail.de>
2020-06-30 16:51:20 +0200

    Add one benchmark
    not sure if this is the best way

---
+++ bench/bench.cpp
@@ -0,0 +1,14 @@
+#include <benchmark/benchmark.h>
+#include <string>
+#include "selectors/selectors.hpp"
+
+static void BM_SelectorParsing(benchmark::State& state) {
+    std::string selector = "\"laptops\"|\"laptop\",\"laptops\"[0]";
+    for (auto _ : state) {
+        parse_selector(selector.begin(), selector.end());
+    }
+}
+
+BENCHMARK(BM_SelectorParsing);
+
+BENCHMARK_MAIN();
